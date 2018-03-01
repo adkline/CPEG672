e248058bea555160728c74c17582b5cdf1c7402a
Anthony Kline <akline@udel.edu>
2018-03-01 00:59:55 +0000

    Figured out how to push this, now to put on githubgi

--- Test Stuff.py
+++
@@ -1,5 +0,0 @@
-import math
-
-
-for letters in range(ord('a'), ord('a')+26):
-    print chr(letters)
--- Untitled1.py
+++
@@ -1,8 +0,0 @@
-import operator, collections
-
-d = {'aaaa' : .001, 'bsbc' : .1, 'cdhf' :.05}
-
-sorted_d = sorted(d.items(), key=operator.itemgetter(0),reverse=True)
-print('Dictionary in descending order by value : ',sorted_d)
-
-print sorted(d.values())
